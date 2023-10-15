c6422d52979128265baa1ea5b17cb3383a2e5bb4
Brian Benedict Njau <85028420+brian-benedict@users.noreply.github.com>
2023-10-15 08:39:36 +0300

    test deployment

--- CMMS_System/settings.py
+++ CMMS_System/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#6@n8&l11r$-xhv*h=_b+3l9p+6jx#b3@vb_@30)*wut6i(n7h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
